23747fad78e2eff8862b9348a731034c1058d2f2
Martin Flower <martin.flower@callistaenterprise.se>
2015-10-15 13:04:12 +0200

    SERVICE-379 do not filter engagements by care unit hsa id

--- GetAggregatedFunctionalStatus/src/main/java/se/skltp/aggregatingservices/riv/clinicalprocess/healthcond/description/getaggregatedfunctionalstatus/RequestListFactoryImpl.java
+++ GetAggregatedFunctionalStatus/src/main/java/se/skltp/aggregatingservices/riv/clinicalprocess/healthcond/description/getaggregatedfunctionalstatus/RequestListFactoryImpl.java
@@ -2,10 +2,9 @@ package se.skltp.aggregatingservices.riv.clinicalprocess.healthcond.description.
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,7 +35,7 @@ public class RequestListFactoryImpl implements RequestListFactory {
                 eiCategorizations.clear();
             } else {
                 for (String eic : eiCategorizations) {
-                    log.info("AggregatedFunctionalStatus - categorization {}", eic);
+                    log.debug("AggregatedFunctionalStatus - categorization {}", eic);
                 }
             }
         }
@@ -46,61 +45,39 @@ public class RequestListFactoryImpl implements RequestListFactory {
     }
 
     /**
-     * Filtrera svarsposter från i EI (ei-engagement) baserat parametrar i GetFunctionalStatus requestet (req). 
-     * Följande villkor måste vara sanna för att en svarspost från EI skall tas med i svaret:
-     * 
-     * 1. req.fromDate <= ei-engagement.mostRecentContent <= req.toDate 
-     * 2. req.careUnitId.size == 0 or req.careUnitId.contains(ei-engagement.logicalAddress)
-     * 
-     * Svarsposter från EI som passerat filtreringen grupperas på fältet sourceSystem 
-     * samt postens fält logicalAddress (= PDL-enhet) samlas i listan careUnitId per varje sourceSystem
-     * 
-     * Ett anrop görs per funnet sourceSystem med följande värden i anropet:
-     * 
-     * 1. logicalAddress  = sourceSystem (systemadressering) 
-     * 2. subjectOfCareId = original-request.subjectOfCareId 
-     * 3. careUnitId      = listan av PDL-enheter som returnerats från EI för aktuellt source system) 
-     * 4. fromDate        = orginal-request.fromDate 5. toDate = orginal-request.toDate
+     * Engagement index has responded with a list of engagements.
+     * Retrieve source system hsa id from the engagement and prepare a list of source systems together with the original request.
+     * This list will be split by mule and sent to each source system.
      */
     public List<Object[]> createRequestList(QueryObject qo, FindContentResponseType src) {
 
-        GetFunctionalStatusType originalRequest = (GetFunctionalStatusType) qo.getExtraArg();
-
-        List<String> reqCareUnitList = null;
-        reqCareUnitList = originalRequest.getCareUnitHSAId();
-        FindContentResponseType eiResp = (FindContentResponseType) src;
-        List<EngagementType> inEngagements = eiResp.getEngagement();
+        FindContentResponseType findContentResponse = (FindContentResponseType) src;
+        List<EngagementType> engagements = findContentResponse.getEngagement();
+        log.debug("Got {} hits in the engagement index", engagements.size());
         
-        log.info("Got {} hits in the engagement index", inEngagements.size());
-
-        Map<String, List<String>> sourceSystem_pdlUnitList_map = new HashMap<String, List<String>>();
-
-        for (EngagementType inEng : inEngagements) {
-            // Filter
-            if (isPartOf(reqCareUnitList, inEng.getLogicalAddress()) 
-                && 
-                isCategorization(inEng.getCategorization())) {
-                // Add pdlUnit to source system
-                log.debug("Add SS: {} for PDL unit: {}", inEng.getSourceSystem(), inEng.getLogicalAddress());
-                addPdlUnitToSourceSystem(sourceSystem_pdlUnitList_map, inEng.getSourceSystem(), inEng.getLogicalAddress());
+        Set<String> sourceSystems = new HashSet<String>(); // set of unique source system hsa ids
+        
+        // remove unsupported categorizations; remove duplicate sourceSystems
+        for (EngagementType engagement : engagements) {
+            if (isCategorization(engagement.getCategorization())) {
+                // If supported categorization - add to set
+                sourceSystems.add(engagement.getSourceSystem());
             }
         }
 
-        // Prepare the result of the transformation as a list of request-payloads.
-        // one payload for each unique logical-address (e.g. source system since we are using system addressing),
-        // each payload built up as an object-array according to the JAX-WS signature for the method in the service interface
-        List<Object[]> reqList = new ArrayList<Object[]>();
+        log.debug("Preparing to call {} different source systems", sourceSystems.size());
+        List<Object[]> requestsToBeSentToSourceSystems = new ArrayList<Object[]>();
+        GetFunctionalStatusType request = (GetFunctionalStatusType) qo.getExtraArg(); // consumer's original request
 
-        for (Entry<String, List<String>> entry : sourceSystem_pdlUnitList_map.entrySet()) {
-            String sourceSystem = entry.getKey();
-            log.info("Calling source system using logical address {} for subject of care id {}", sourceSystem, originalRequest.getPatientId() == null ? null : originalRequest.getPatientId().getId());
-            GetFunctionalStatusType request = originalRequest;
+        for (String sourceSystem : sourceSystems) {
+            log.info("Preparing to call source system {} for subject of care id {}", sourceSystem, request.getPatientId() == null ? null : request.getPatientId().getId());
+            // the original request is sent unchanged to each sourceSystem
             Object[] reqArr = new Object[] { sourceSystem, request };
-            reqList.add(reqArr);
+            requestsToBeSentToSourceSystems.add(reqArr);
         }
 
-        log.debug("Transformed payload: {}", reqList);
-        return reqList;
+        log.debug("Transformed payload: {}", requestsToBeSentToSourceSystems);
+        return requestsToBeSentToSourceSystems;
     }
 
     private boolean isCategorization(String categorization) {
@@ -113,23 +90,5 @@ public class RequestListFactoryImpl implements RequestListFactory {
             log.debug("Rejected unsupported categorization {}", categorization);
             return false;
         }
-    }
-
-    protected boolean isPartOf(List<String> careUnitIdList, String careUnit) {
-        log.debug("Check presence of {} in {}", careUnit, careUnitIdList);
-        if (careUnitIdList == null || careUnitIdList.isEmpty()) {
-            return true;
-        } else {
-            return careUnitIdList.contains(careUnit);
-        }
-    }
-
-    private void addPdlUnitToSourceSystem(Map<String, List<String>> sourceSystem_pdlUnitList_map, String sourceSystem, String pdlUnitId) {
-        List<String> careUnitList = sourceSystem_pdlUnitList_map.get(sourceSystem);
-        if (careUnitList == null) {
-            careUnitList = new ArrayList<String>();
-            sourceSystem_pdlUnitList_map.put(sourceSystem, careUnitList);
-        }
-        careUnitList.add(pdlUnitId);
     }
 }
